48d6a5da0870f8c519938192f17ca53addfbb548
ecorona9 <79238407+ecorona9@users.noreply.github.com>
2022-04-23 05:24:29 -0700

    Good adjustments and sizing, removed borders

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -16,8 +16,8 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 function Home() {
 
   return (
-    <>
-        <Parallax pages={5}>
+    <div className='home'>
+        <Parallax pages={3.13}>
             <ParallaxLayer 
                     offset={0.2}
                     speed={0.1}
@@ -33,86 +33,86 @@ function Home() {
             </ParallaxLayer>
 
             <ParallaxLayer 
-                offset={1}
+                offset={0.9}
                 speed={0.3}
                 style={{
                 backgroundImage: `url(${back})`,
                 backgroundSize: 'cover',
-                border: '3px solid blue'
                 }}
             />
 
             <ParallaxLayer
-            offset={1.4}
+            offset={1.1}
             speed={0.6}
-            factor={1.5}
+            factor={1.1}
             style={{
                 backgroundImage: `url(${mid})`,
                 backgroundSize: 'cover',
-                border: '3px solid green'
             }}
             />
 
             <ParallaxLayer
-            offset={1.6}
+            offset={1.2}
             speed={0.7}
-            factor={2}
+            factor={1}
             style={{
                 backgroundImage: `url(${front})`,
                 backgroundSize: 'cover',
-                border: '3px solid white'
             }}
             />
             <ParallaxLayer
-            offset={1.6}
+            offset={1.95}
             speed={1}
-            factor={1}
+            factor={2}
             style={{
-                border: '3px solid pink',
                 display: 'flex',
                 alignItems: 'center',
-                flexDirection: 'column'
+                flexDirection: 'column',
+                backgroundColor: 'black'
             }}
             >
-                <h1>AVAILABLE VERY SOON...</h1>
-                <h1 className='platform'>💻 WINDOWS MAC LINUX</h1>
-                <div>
-                    <iframe width="1080" height="720" src="https://www.youtube.com/embed/cXWlgP5hZzc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                <div className='synopsis'>
-                    <div className='main'>
-                        Midnight Metal Madness is a shoot'em up shooter video game. You're a detective working in the
-                        year 2149 the streets of the city of TunG_zten roam with metallic crime. Your job? Shut them down
-                        and the keep the city safe! Corruption runs deep in the city and you're pulling the strings together.
+                <div className='definition'>
+                    <h1>AVAILABLE  VERY SOON...</h1>
+                    <h1 className='platform'>💻 WINDOWS MAC LINUX</h1>
+                    <div>
+                        <iframe width="1080" height="720" src="https://www.youtube.com/embed/cXWlgP5hZzc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
-                    <div className='columns'>
-                        <div>
-                            "ARGHHH! I've been working for so long I can't seem to remember my name... All I know is that the city
-                            hired me to keep the streets safe but something just feels off. I feel like I know something that's 
-                            really important... I just can't quite remember." As a highly trained criminal investigator for the department of SIGMA TROOP 21. The city 
-                            has equipped you with some of the best technology and weapons to keep the streets safe.
+                    <div className='synopsis'>
+                        <div className='main'>
+                            Midnight Metal Madness is a shoot'em up shooter video game. You're a detective working in the
+                            year 2149 the streets of the city of TunG_zten roam with metallic crime. Your job? Shut them down
+                            and the keep the city safe! Corruption runs deep in the city and you're pulling the strings together.
                         </div>
-                        <div>
-                             It's common
-                            to encounter rogue computer personell. There has been an increase in reports of violent robotic personall
-                            attacking civilians. Our intelligence department has reported that someone has obtained the RSA encryption keys for the
-                            software operative standars on robots and machines. Most likely a hacker working for the notorious ARKA1D group.
+                        <div className='columns'>
+                            <div>
+                                "ARGHHH! I've been working for so long I can't seem to remember my name... All I know is that the city
+                                hired me to keep the streets safe but something just feels off. I feel like I know something that's 
+                                really important... I just can't quite remember." As a highly trained criminal investigator for the department of SIGMA TROOP 21. The city 
+                                has equipped you with some of the best technology and weapons to keep the streets safe.
+                            </div>
+                            <div>
+                                It's common
+                                to encounter rogue computer personell. There has been an increase in reports of violent robotic personall
+                                attacking civilians. Our intelligence department has reported that someone has obtained the RSA encryption keys for the
+                                software operative standars on robots and machines. Most likely a hacker working for the notorious ARKA1D group.
+                            </div>
                         </div>
                     </div>
+                    <div className='gameplayPics'>
+                        <img src={gameplay1} alt="" />
+                        <img src={gameplay2} alt="" />
+                        <img src={gameplay3} alt="" />
+                        <img src={gameplay4} alt="" />
+                    </div>
+                    <div className='footer'>
+                        Made by students attending CSUF 2022.
+                    </div>
                 </div>
-                <div className='gameplayPics'>
-                    <img src={gameplay1} alt="" />
-                    <img src={gameplay2} alt="" />
-                    <img src={gameplay3} alt="" />
-                    <img src={gameplay4} alt="" />
-                </div>
-                <div className='footer'>
-                    Made by students attending CSUF 2022.
-                </div>
+                
                 
             </ParallaxLayer>
         </Parallax>
-    </>
+    </div>
   );
 }
 
